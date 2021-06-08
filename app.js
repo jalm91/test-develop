e47fa5926e3c2ce67f6fb2026e73fd7f6c8bbc26
home <jalm91@live.com>
2021-06-08 01:08:37 -0500

    feat: 🎸 price of parking
    price of every parking depends of size and fuel

---
+++ app.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const path = require('path');
+const { fileDataName, fileDataPath, defaultMinimumPercentCapacity, fuelFixedRate } = require('./constants/constants')
+
+
+const readFile = () => {
+    try {
+        const fullPathDataFile = path.join('./', fileDataPath, fileDataName);
+        const dataBuffer = fs.readFileSync(fullPathDataFile);
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return []
+    }
+};
+const getPriceOfParking = (vehicle) => {
+    const size = { large: 35, small: 25 };
+    return size[vehicle.size];
+};
+
+const merge = () => {
+    const vehicles = readFile();
+    const vehiclesWithPriceRaw = vehicles.map(vehicle => {
+        const { licencePlate } = vehicle;
+        const priceService = fullPrice(vehicle);
+        return {
+            licencePlate,
+            ...priceService,
+        }
+    });
+    vehiclesWithPriceRaw.sort(sortVehiclesByPrice);
+    console.log(vehiclesWithPriceRaw);
+}
+
+const getFuelPercentage = (capacity, level) => {
+    const percentage = (level * 100) / capacity;
+    return percentage;
+}
+
+const getFuelToCompete = (capacity, level) => {
+    return capacity - level;
+}
+
+const getFuelToCompetePrice = (fuel) => {
+    return fuel * fuelFixedRate
+}
+const fullPrice = (vehicle) => {
+    const { capacity, level } = vehicle.fuel;
+    const priceOfParking = getPriceOfParking(vehicle);
+    const fuelPercentage = getFuelPercentage(capacity, level);
+    let fuelAdded = 0;
+    if (fuelPercentage <= defaultMinimumPercentCapacity) {
+        fuelAdded = getFuelToCompete(capacity, level);
+        const fuelToCompetePrice = getFuelToCompetePrice(fuelAdded);
+        const price = fuelToCompetePrice + priceOfParking;
+        return { price, fuelAdded };
+    }
+    return { price: priceOfParking, fuelAdded };
+}
+
+const sortVehiclesByPrice = (a, b) => {
+    return a.price > b.price;
+}
+
+const splitToAgents = (vehicle) => {
+    while
+}
+
+merge();
---
+++ constants/constants.js
@@ -0,0 +1,4 @@
+exports.fileDataName = "data.json";
+exports.fileDataPath = "data";
+exports.defaultMinimumPercentCapacity = 10;
+exports.fuelFixedRate = 1.75;
