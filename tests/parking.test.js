90f50b02840908d59e87cf688fe23b405305db24
home <jalm91@live.com>
2021-06-08 22:00:43 -0500

    style: 💄 add describe to test
    to improve readability now there is describe inside test

--- tests/parking.test.js
+++ tests/parking.test.js
@@ -11,251 +11,265 @@ const { getFuelPercentage, getFuelToCompete,
     readFile, fullPrice,
 } = require('../lib/parking');
 
+describe('then getPriceOfParking', () => {
 
-test('getPriceOfParking price of parking small vehicles sould be 25', () => {
-    const vehicle = { size: 'small' };
-    expect(getPriceOfParking(vehicle)).toBe(smallPrice)
+    test('getPriceOfParking price of parking small vehicles sould be 25', () => {
+        const vehicle = { size: 'small' };
+        expect(getPriceOfParking(vehicle)).toBe(smallPrice)
+    });
+
+    test('getPriceOfParking price of parking small vehicles sould be 35', () => {
+        const vehicle = { size: 'large' };
+        expect(getPriceOfParking(vehicle)).toBe(largePrice)
+    });
+})
+describe('then getFuelPercentage', () => {
+    test('getFuelPercentage 100 capacity and 10 level should be 10 percent', () => {
+        expect(getFuelPercentage(100, 10)).toBe(10);
+    })
+
+    test('getFuelPercentage when 0 capacity', () => {
+        const error = new Error('Invalida Capacity')
+        const functionWithError = () => {
+            getFuelPercentage(0, 10)
+        }
+        expect(functionWithError).toThrowError('Invalida Capacity');
+    })
 })
 
-test('getPriceOfParking price of parking small vehicles sould be 35', () => {
-    const vehicle = { size: 'large' };
-    expect(getPriceOfParking(vehicle)).toBe(largePrice)
+describe('then getFuelToCompete ', () => {
+    test('getFuelToCompete 65 fuel and 10 level should be 55 ', () => {
+        expect(getFuelToCompete(65, 10)).toBe(55);
+    })
+
+    test('getFuelToCompete when is less than 0 should be 0 ', () => {
+        expect(getFuelToCompete(65, 100)).toBe(0);
+    })
+
+    test('getFuelToCompete when the capacity is equal to level ', () => {
+        expect(getFuelToCompete(10, 10)).toBe(0);
+    })
+
 })
 
-test('getFuelPercentage 100 capacity and 10 level should be 10 percent', () => {
-    expect(getFuelPercentage(100, 10)).toBe(10);
+describe('then getFuelToCompetePrice ', () => {
+    test('getFuelToCompetePrice to 10 fuel should be 17.5', () => {
+        expect(getFuelToCompetePrice(10)).toBe(17.5);
+    })
 })
 
-test('getFuelPercentage when 0 capacity', () => {
-    const error = new Error('Invalida Capacity')
-    const functionWithError = () => {
-        getFuelPercentage(0, 10)
-    }
-    expect(functionWithError).toThrowError('Invalida Capacity');
-})
-
-test('getFuelToCompete 65 fuel and 10 level should be 55 ', () => {
-    expect(getFuelToCompete(65, 10)).toBe(55);
-})
-
-test('getFuelToCompete when is less than 0 should be 0 ', () => {
-    expect(getFuelToCompete(65, 100)).toBe(0);
-})
-
-test('getFuelToCompete when the capacity is equal to level ', () => {
-    expect(getFuelToCompete(10, 10)).toBe(0);
-})
-
-test('getFuelToCompetePrice to 10 fuel should be 17.5', () => {
-    expect(getFuelToCompetePrice(10)).toBe(17.5);
-})
-
-test('Should be return an array sorted', () => {
-    const vehicles = [{
-        licencePlate: 'E',
-        employee: 'A',
-        fuelAdded: 93.8,
-        price: 199.15
-    }, {
-        licencePlate: 'D',
-        employee: 'A',
-        fuelAdded: 78.07,
-        price: 171.6225
-    }]
-    const sortedVehicles = [
-        {
-            licencePlate: 'D',
-            employee: 'A',
-            fuelAdded: 78.07,
-            price: 171.6225
-        }, {
+describe('then split vehicles', () => {
+    test('Should be return an array sorted', () => {
+        const vehicles = [{
             licencePlate: 'E',
             employee: 'A',
             fuelAdded: 93.8,
             price: 199.15
-        }
-    ]
-    expect(vehicles.sort(sortVehiclesByPrice)).toStrictEqual(sortedVehicles);
-})
-
-test('Sould be split To Employs equal', () => {
-    const vehiclesWithoutEmployee = [
-        {
+        }, {
             licencePlate: 'D',
+            employee: 'A',
             fuelAdded: 78.07,
             price: 171.6225
-        }, {
-            licencePlate: 'E',
-            fuelAdded: 93.8,
-            price: 199.15
-        }
-    ]
-    const vehiclesWithEmployee = [
-        {
-            licencePlate: 'D',
-            employee: 'B',
-            fuelAdded: 78.07,
-            price: 171.6225
-        }, {
-            licencePlate: 'E',
-            employee: 'A',
-            fuelAdded: 93.8,
-            price: 199.15
-        }
-    ]
-    expect(splitToEmpoys(vehiclesWithoutEmployee)).toStrictEqual(vehiclesWithEmployee);
-})
-
-test('Sould be split To Minimal Cost', () => {
-    const vehiclesWithoutEmployee = [
-        {
-            licencePlate: 'A',
-            fuelAdded: 10,
-            price: 42.5
-        }, {
-            licencePlate: 'B',
-            fuelAdded: 20,
-            price: 70
-        },
-        {
-            licencePlate: 'C',
-            fuelAdded: 15,
-            price: 51.25
-        }
-    ]
-    const vehiclesWithEmployee = [
-        {
-            employee: 'B',
-            licencePlate: 'A',
-            fuelAdded: 10,
-            price: 42.5
-        },
-        {
-            employee: 'A',
-            licencePlate: 'C',
-            fuelAdded: 15,
-            price: 51.25
-        },
-        {
-            employee: 'A',
-            licencePlate: 'B',
-            fuelAdded: 20,
-            price: 70
-        },
-    ]
-    vehiclesWithoutEmployee.sort(sortVehiclesByPrice)
-    expect(splitToEmpoys(vehiclesWithoutEmployee)).toStrictEqual(vehiclesWithEmployee);
-})
-
-test('fullPrice to large size with less than 10 percentaje of level', () => {
-    const vehicle = {
-        licencePlate: "A",
-        size: "large",
-        fuel: {
-            capacity: 10,
-            level: 0.01
-        }
-    };
-    const fuelAdded = (vehicle.fuel.capacity - vehicle.fuel.level);
-    const price = fuelAdded * fuelFixedRate + largePrice;
-    const expectedPrice = { fuelAdded, price };
-    expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
-})
-
-test('fullPrice to large size with more than 10 percentaje of level', () => {
-    const vehicle = {
-        licencePlate: "A",
-        size: "large",
-        fuel: {
-            capacity: 10,
-            level: 2
-        }
-    };
-    const fuelAdded = 0;
-    const price = largePrice;
-    const expectedPrice = { fuelAdded, price };
-    expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
-})
-
-test('fullPrice to small size with less than 10 percentaje of level', () => {
-    const vehicle = {
-        licencePlate: "A",
-        size: "small",
-        fuel: {
-            capacity: 10,
-            level: 0.01
-        }
-    };
-    const fuelAdded = (vehicle.fuel.capacity - vehicle.fuel.level);
-    const price = fuelAdded * fuelFixedRate + smallPrice;
-    const expectedPrice = { fuelAdded, price };
-    expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
-})
-
-test('fullPrice to small size with more than 10 percentaje of level', () => {
-    const vehicle = {
-        licencePlate: "A",
-        size: "small",
-        fuel: {
-            capacity: 10,
-            level: 2
-        }
-    };
-    const fuelAdded = 0;
-    const price = smallPrice;
-    const expectedPrice = { fuelAdded, price };
-    expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
-})
-
-test('should be return a readFile', () => {
-
-    const vehicles = [
-        {
-            "licencePlate": "A",
-            "size": "large",
-            "fuel": {
-                "capacity": 57,
-                "level": 0.07
+        }]
+        const sortedVehicles = [
+            {
+                licencePlate: 'D',
+                employee: 'A',
+                fuelAdded: 78.07,
+                price: 171.6225
+            }, {
+                licencePlate: 'E',
+                employee: 'A',
+                fuelAdded: 93.8,
+                price: 199.15
             }
-        },
-        {
-            "licencePlate": "B",
-            "size": "large",
-            "fuel": {
-                "capacity": 66,
-                "level": 0.59
+        ]
+        expect(vehicles.sort(sortVehiclesByPrice)).toStrictEqual(sortedVehicles);
+    })
+
+    test('Sould be split To Employs equal', () => {
+        const vehiclesWithoutEmployee = [
+            {
+                licencePlate: 'D',
+                fuelAdded: 78.07,
+                price: 171.6225
+            }, {
+                licencePlate: 'E',
+                fuelAdded: 93.8,
+                price: 199.15
             }
-        }];
-    const vehiclesToString = JSON.stringify(vehicles);
-    const vehiclesBuffer = Buffer.from(vehiclesToString)
-    fs.readFileSync.mockImplementation(() => vehiclesBuffer)
-    expect(readFile()).toEqual(vehicles);
+        ]
+        const vehiclesWithEmployee = [
+            {
+                licencePlate: 'D',
+                employee: 'B',
+                fuelAdded: 78.07,
+                price: 171.6225
+            }, {
+                licencePlate: 'E',
+                employee: 'A',
+                fuelAdded: 93.8,
+                price: 199.15
+            }
+        ]
+        expect(splitToEmpoys(vehiclesWithoutEmployee)).toStrictEqual(vehiclesWithEmployee);
+    })
+
+    test('Sould be split To Minimal Cost', () => {
+        const vehiclesWithoutEmployee = [
+            {
+                licencePlate: 'A',
+                fuelAdded: 10,
+                price: 42.5
+            }, {
+                licencePlate: 'B',
+                fuelAdded: 20,
+                price: 70
+            },
+            {
+                licencePlate: 'C',
+                fuelAdded: 15,
+                price: 51.25
+            }
+        ]
+        const vehiclesWithEmployee = [
+            {
+                employee: 'B',
+                licencePlate: 'A',
+                fuelAdded: 10,
+                price: 42.5
+            },
+            {
+                employee: 'A',
+                licencePlate: 'C',
+                fuelAdded: 15,
+                price: 51.25
+            },
+            {
+                employee: 'A',
+                licencePlate: 'B',
+                fuelAdded: 20,
+                price: 70
+            },
+        ]
+        vehiclesWithoutEmployee.sort(sortVehiclesByPrice)
+        expect(splitToEmpoys(vehiclesWithoutEmployee)).toStrictEqual(vehiclesWithEmployee);
+    })
 })
 
-test('should be control an error inside readFile function', () => {
+describe('then fullPrice', () => {
+    test('fullPrice to large size with less than 10 percentaje of level', () => {
+        const vehicle = {
+            licencePlate: "A",
+            size: "large",
+            fuel: {
+                capacity: 10,
+                level: 0.01
+            }
+        };
+        const fuelAdded = (vehicle.fuel.capacity - vehicle.fuel.level);
+        const price = fuelAdded * fuelFixedRate + largePrice;
+        const expectedPrice = { fuelAdded, price };
+        expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
+    })
 
-    const vehicles = [
-        {
-            "licencePlate": "A",
-            "size": "large",
-            "fuel": {
-                "capacity": 57,
-                "level": 0.07
+    test('fullPrice to large size with more than 10 percentaje of level', () => {
+        const vehicle = {
+            licencePlate: "A",
+            size: "large",
+            fuel: {
+                capacity: 10,
+                level: 2
             }
-        },
-        {
-            "licencePlate": "B",
-            "size": "large",
-            "fuel": {
-                "capacity": 66,
-                "level": 0.59
+        };
+        const fuelAdded = 0;
+        const price = largePrice;
+        const expectedPrice = { fuelAdded, price };
+        expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
+    })
+
+    test('fullPrice to small size with less than 10 percentaje of level', () => {
+        const vehicle = {
+            licencePlate: "A",
+            size: "small",
+            fuel: {
+                capacity: 10,
+                level: 0.01
             }
-        }];
-    const vehiclesToString = JSON.stringify(vehicles);
-    const errMock = new Error('Controled error');
-    fs.readFileSync.mockImplementation(() => errMock)
-    const funcWithErr = () => {
-        readFile()
-    }
-    expect(funcWithErr).toThrowError();
+        };
+        const fuelAdded = (vehicle.fuel.capacity - vehicle.fuel.level);
+        const price = fuelAdded * fuelFixedRate + smallPrice;
+        const expectedPrice = { fuelAdded, price };
+        expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
+    })
+
+    test('fullPrice to small size with more than 10 percentaje of level', () => {
+        const vehicle = {
+            licencePlate: "A",
+            size: "small",
+            fuel: {
+                capacity: 10,
+                level: 2
+            }
+        };
+        const fuelAdded = 0;
+        const price = smallPrice;
+        const expectedPrice = { fuelAdded, price };
+        expect(fullPrice(vehicle)).toStrictEqual(expectedPrice);
+    })
+})
+
+describe('When read data file', () => {
+    test('should be return a readFile', () => {
+
+        const vehicles = [
+            {
+                "licencePlate": "A",
+                "size": "large",
+                "fuel": {
+                    "capacity": 57,
+                    "level": 0.07
+                }
+            },
+            {
+                "licencePlate": "B",
+                "size": "large",
+                "fuel": {
+                    "capacity": 66,
+                    "level": 0.59
+                }
+            }];
+        const vehiclesToString = JSON.stringify(vehicles);
+        const vehiclesBuffer = Buffer.from(vehiclesToString)
+        fs.readFileSync.mockImplementation(() => vehiclesBuffer)
+        expect(readFile()).toEqual(vehicles);
+    })
+
+    test('should be control an error inside readFile function', () => {
+
+        const vehicles = [
+            {
+                "licencePlate": "A",
+                "size": "large",
+                "fuel": {
+                    "capacity": 57,
+                    "level": 0.07
+                }
+            },
+            {
+                "licencePlate": "B",
+                "size": "large",
+                "fuel": {
+                    "capacity": 66,
+                    "level": 0.59
+                }
+            }];
+        const vehiclesToString = JSON.stringify(vehicles);
+        const errMock = new Error('Controled error');
+        fs.readFileSync.mockImplementation(() => errMock)
+        const funcWithErr = () => {
+            readFile()
+        }
+        expect(funcWithErr).toThrowError();
+    })
 })
