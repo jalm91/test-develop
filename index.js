0f279ea6c55b7333cd82453d8faad9ba2943eee6
home <jalm91@live.com>
2021-06-08 21:05:09 -0500

    style: 💄 add a new styles to data
    add a new files to styles

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const yargs = require('yargs');
 const app = require('./lib/parking');
-
+const chalk = require('chalk');
+const log = console.log;
 const merge = () => {
-    const vehicles = app.readFile();
-    const vehiclesWithPriceRaw = vehicles.map(vehicle => {
-        const { licencePlate } = vehicle;
-        const priceService = app.fullPrice(vehicle);
-        return {
-            licencePlate,
-            ...priceService,
-        }
-    });
-    vehiclesWithPriceRaw.sort(app.sortVehiclesByPrice);
-    const vehiclesWithEmployee = app.splitToEmpoys(vehiclesWithPriceRaw);
-    console.log(vehiclesWithEmployee)
+    try {
+        const vehicles = app.readFile();
+        const vehiclesWithPriceRaw = vehicles.map(vehicle => {
+            const { licencePlate } = vehicle;
+            const priceService = app.fullPrice(vehicle);
+            return {
+                licencePlate,
+                ...priceService,
+            }
+        });
+        vehiclesWithPriceRaw.sort(app.sortVehiclesByPrice);
+        const vehiclesWithEmployee = app.splitToEmpoys(vehiclesWithPriceRaw);
+        log(chalk.green("Vehicles and parking...."))
+        vehiclesWithEmployee.forEach(vehicle => { 
+            log(vehicle)
+        })
 
+    } catch (err) {
+        log(chalk.bold.red(err))
+    }
 }
 
 yargs.command({
